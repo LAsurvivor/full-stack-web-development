987c7c187fb0dac90119f8eb3d7f2351e8909199
LAsurvivor1787 <lasurvivor1787@gmail.com>
2022-03-11 19:43:54 +0800

    Assignment3-Course2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentFormComponent';
 
     function RenderDish({dish}) {
         return(
@@ -59,6 +60,7 @@ import { Link } from 'react-router-dom';
                         </div>
                         <div className="col-12 col-md-5 m-1">
                             <RenderComments comments={props.comments} />
+                            <CommentForm />
                         </div>
                     </div>
                 </div> 
