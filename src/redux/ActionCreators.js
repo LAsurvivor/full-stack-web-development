b8e6b00bbde0f82193e7f999c4b0fe18bcf070df
LAsurvivor1787 <lasurvivor1787@gmail.com>
2022-03-13 18:44:57 +0800

    Fetch Post Comment (Debugged)

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -8,41 +8,39 @@ export const addComment = (comment) => ({
 });
 
 export const postComment = (dishId, rating, author, comment) => (dispatch) => {
+
     const newComment = {
         dishId: dishId,
         rating: rating,
         author: author,
         comment: comment
-    }
-    newComment.date = new Date().toISOString;
-
-    return fetch(baseUrl + 'dishes', {
-        method: 'POST',
+    };
+    newComment.date = new Date().toISOString();
+    
+    return fetch(baseUrl + 'comments', {
+        method: "POST",
         body: JSON.stringify(newComment),
         headers: {
-            'Content-Type': 'application/json'
+          "Content-Type": "application/json"
         },
-        credentials: 'same-origin'
+        credentials: "same-origin"
     })
     .then(response => {
         if (response.ok) {
-            return response;
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
         }
-        else {
-            var error = new Error('Error ' + response.status + ': ' + response.statusText);
-            error.response = response;
+      },
+      error => {
             throw error;
-        }
-    },
-    error => {
-        var errmess = new Error(error.message);
-        throw errmess;
-    })
+      })
     .then(response => response.json())
     .then(response => dispatch(addComment(response)))
-    .catch(error => { console.log('Post comment ', error.message)
-        alert('Your comment could not be posted\nError: ' + error.message); })
-}
+    .catch(error =>  { console.log('post comments', error.message); alert('Your comment could not be posted\nError: '+error.message); });
+};
 
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
